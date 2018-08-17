b3b6762a949c82d810a682f4a2962ff678978117
qmlab <mainchin@msn.com>
2018-08-17 14:58:43 -0700

    Add xhp package

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?hh
+require_once(__DIR__."/../vendor/autoload.php");
+$a = 1;
+echo
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <title>REIS Mortgage Caldulator</title>
+    </head>
+    <body>
+      Where to go next? {$a}
+    </body>
+  </html>;
