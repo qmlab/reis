f97f347683aaba1e8b230c24816cd2df6b4277b0
qmlab <mainchin@msn.com>
2018-08-19 21:10:59 +0000

    Fix bug in ROI calculator

--- calculators/roi_calculator.php
+++ calculators/roi_calculator.php
@@ -1,15 +1,16 @@
-<?hh // strict
+<?hh // partial
+require_once(__DIR__."/mortgage_calculator.php");
 
 class RentalROICalculator {
   private MortgageCalculator $mc;
-  public function __construct(private float $total, private float $downpay_ratio, private float $interest_rate, private float $insurance, private float $property_tax_rate, private float $pmi_rate, private int $months, private int $months_pmi, private float $fixing_cost, private float $estimated_rental, private float $property_management_rate, private float $maintenance_rate, private float $holding_months, private int $months_rental) {
+  public function __construct(private float $total, private float $downpay_ratio, private float $interest_rate, private float $insurance, private float $property_tax_rate, private float $pmi_rate, private int $months, private int $months_pmi, private float $fixing_cost, private float $estimated_rental, private float $property_management_rate, private float $maintenance_rate, private int $holding_months, private int $months_rental) {
     $this->mc = new MortgageCalculator($total, $downpay_ratio, $interest_rate, $insurance, $property_tax_rate, $pmi_rate, $months, $months_pmi);
   }
 
   // Monthly cash flow
   public function getCashFlow(): float {
     $payment = $this->mc->getPayment();
-    $cash = ($this->$estimated_rental * (1 - $this->property_management_rate / 100 - $this->maintenance_rate / 100) * ($this->months_rental - $this->holding_months) - $payment * $this->months_rental - $this->fixing_cost) / $this->months_rental;
+    $cash = ($this->estimated_rental * (1 - $this->property_management_rate / 100 - $this->maintenance_rate / 100) * ($this->months_rental - $this->holding_months) - $payment * $this->months_rental - $this->fixing_cost) / $this->months_rental;
     return $cash;
   }
 
@@ -22,7 +23,7 @@ class RentalROICalculator {
 
 class SellingROICalculator {
   private MortgageCalculator $mc;
-  public function __construct(private float $total, private float $downpay_ratio, private float $interest_rate, private float $insurance, private float $property_tax_rate, private float $pmi_rate, private int $months, private int $months_pmi, private float $fixing_cost, private float $estimated_price, private float $commission_rate, private float $excise_tax_rate, private float $closing_cost, private float $holding_months) {
+  public function __construct(private float $total, private float $downpay_ratio, private float $interest_rate, private float $insurance, private float $property_tax_rate, private float $pmi_rate, private int $months, private int $months_pmi, private float $fixing_cost, private float $estimated_price, private float $commission_rate, private float $excise_tax_rate, private float $closing_cost, private int $holding_months) {
     $this->mc = new MortgageCalculator($total, $downpay_ratio, $interest_rate, $insurance, $property_tax_rate, $pmi_rate, $months, $months_pmi);
   }
 
