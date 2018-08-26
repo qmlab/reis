58b8db4b4a2939126e6ddc2288e7da8427d14d76
Mian Qin <mian@fb.com>
2018-08-25 19:36:59 -0700

    Combine two calculator pages into one

--- www/sale_roi.php
+++ www/sale_roi.php
@@ -2,7 +2,7 @@
 require_once(__DIR__."/../calculators/roi_calculator.php");
 require_once(__DIR__."/../vendor/hh_autoload.php");
 
-function getSellingROI() {
+function get_sale_roi() {
   $rc = new SellingROICalculator((float)$_POST['total'], (float)$_POST['downpay_ratio'], (float)$_POST['interest_rate'], (float)$_POST['insurance'], (float)$_POST['property_tax_rate'], (float)$_POST['pmi_rate'], (int)$_POST['months'], (int)$_POST['months_pmi'], (float)$_POST['fixing_cost'], (float)$_POST['estimated_price'], (float)$_POST['commission_rate'], (float)$_POST['excise_tax_rate'], (float)$_POST['closing_cost'], (int)$_POST['holding_months']);
   $roi = round($rc->getROI() * 100, 2);
   $yearly = round($rc->getYearlyROI() * 100, 2);
@@ -13,4 +13,4 @@ function getSellingROI() {
 	  </div>;
 }
 
-getSellingROI();
+get_sale_roi();
