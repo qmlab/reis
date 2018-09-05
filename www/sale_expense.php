6e0dee0d1371fe77ca75ed3f10c014406554bbce
qmlab <mainchin@msn.com>
2018-09-04 23:25:12 -0700

    Format changes

--- www/sale_expense.php
+++ www/sale_expense.php
@@ -4,7 +4,7 @@ require_once(__DIR__."/../vendor/hh_autoload.php");
 
 function get_sale_expense() {
   $rc = new SellingROICalculator((float)$_POST['total'], (float)$_POST['downpay_ratio'], (float)$_POST['interest_rate'], (float)$_POST['insurance'], (float)$_POST['property_tax_rate'], (float)$_POST['pmi_rate'], (int)$_POST['months'], (int)$_POST['months_pmi'], (float)$_POST['fixing_cost'], (float)$_POST['estimated_price'], (float)$_POST['commission_rate'], (float)$_POST['excise_tax_rate'], (float)$_POST['closing_cost'], (int)$_POST['holding_months'], (float)$_POST['hoa']);
-  $exp = round($rc->getExpense(), 2);
+  $exp = number_format($rc->getExpense(), 2, ".", ",");
   echo
 	  <div>
 	   <strong>${$exp}</strong>
