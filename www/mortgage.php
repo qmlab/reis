57a22beff0d5db89b8f686d8abc97b5a1e503200
qmlab <mainchin@msn.com>
2018-09-09 15:36:24 -0700

    Adjust number format and remove linked values

--- www/mortgage.php
+++ www/mortgage.php
@@ -4,7 +4,7 @@ require_once(__DIR__."/../vendor/hh_autoload.php");
 
 function getPayment() {
   $mc = new MortgageCalculator((float)$_POST['total'], (float)$_POST['downpay_ratio'], (float)$_POST['interest_rate'], (float)$_POST['insurance'], (float)$_POST['property_tax_rate'], (float)$_POST['pmi_rate'], (int)$_POST['months'], (int)$_POST['months_pmi'], (float)$_POST['hoa']);
-  $payment = round($mc->getPayment(), 2);
+  $payment = number_format($mc->getPayment(), 0, ".", ",");
   echo
     <strong>${$payment}</strong>;
 }
--- www/rental_expense.php
+++ www/rental_expense.php
@@ -4,7 +4,7 @@ require_once(__DIR__."/../vendor/hh_autoload.php");
 
 function get_rental_expense() {
   $rc = new RentalROICalculator((float)$_POST['total'], (float)$_POST['downpay_ratio'], (float)$_POST['interest_rate'], (float)$_POST['insurance'], (float)$_POST['property_tax_rate'], (float)$_POST['pmi_rate'], (int)$_POST['months'], (int)$_POST['months_pmi'], (float)$_POST['fixing_cost'], (float)$_POST['estimated_rental'], (float)$_POST['property_management_rate'], (float)$_POST['maintenance_rate'], (int)$_POST['holding_months'], (int)$_POST['months_rental'], (float)$_POST['hoa']);
-  $exp = number_format($rc->getExpense(), 2, ".", ",");
+  $exp = number_format($rc->getExpense(), 0, ".", ",");
   echo
 	  <div>
 	   <strong>${$exp}</strong>
